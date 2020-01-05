ceb5a4bb2041fbe0e80834d0ac3b5d7af1f7ec37
shivaluma <shivaluma@gmail.com>
2020-01-06 00:51:43 +0700

    replace master by shiro

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    res.redirect('/login');
+    return res.redirect('/login');
   }
   next();
 };
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,8 @@
 var express = require('express');
 const shopController = require('../controller/shop');
 var router = express.Router();
-
+var isAuth = require('../middleware/is-auth');
 /* GET home page. */
-router.get('/add', shopController.getAddNewProduct);
-router.post('/add', shopController.postAddNewProduct);
+router.get('/add',isAuth, shopController.getAddNewProduct);
+router.post('/add',isAuth, shopController.postAddNewProduct);
 module.exports = router;
